76f5840584b6411b38912ae4bfcc16c613fe12e0
Patrick Smith <patrick@burntcaramel.com>
2017-11-28 22:25:23 +1100

    Add link to GitHub

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import { difficulties } from './models/values'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <Game { ...difficulties.intermediate } />
+        <nav className='links-nav'>
+          <a href='https://github.com/RoyalIcing/minesweeter'>Fork on GitHub</a>
+        </nav>
       </div>
     );
   }
